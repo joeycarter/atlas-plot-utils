57bdd226171f5b3bb75e6b597410a5a6be4e859b
Joey Carter <joey.snarrcarter@gmail.com>
2017-10-16 22:16:39 +0200

    Fix formatting in random_hist example

--- examples/random_hist.py
+++ examples/random_hist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Random Histogram
 ================
@@ -12,12 +13,6 @@ from __future__ import absolute_import, division, print_function
 
 import ROOT as root
 
-import sys
-import os.path
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
-)
-
 from atlasplots import atlas_style as astyle
 
 
@@ -26,21 +21,21 @@ def main():
     astyle.SetAtlasStyle()
 
     # Construct the canvas
-    c1 = root.TCanvas('c1', 'The FillRandom example', 0, 0, 800, 600)
+    c1 = root.TCanvas("c1", "The FillRandom example", 0, 0, 800, 600)
 
     # Define a distribution
-    form1 = root.TFormula('form1', 'abs(sin(x)/x)')
-    sqroot = root.TF1('sqroot', 'x*gaus(0) + [3]*form1', 0, 10)
+    form1 = root.TFormula("form1", "abs(sin(x)/x)")
+    sqroot = root.TF1("sqroot", "x*gaus(0) + [3]*form1", 0, 10)
     sqroot.SetParameters(10, 4, 1, 20)
 
     # Randomly fill the histrogram according to the above distribution
-    hist = root.TH1F('hist', 'Test random numbers', 100, 0, 10)
-    hist.FillRandom('sqroot', 10000)
+    hist = root.TH1F("hist", "Test random numbers", 100, 0, 10)
+    hist.FillRandom("sqroot", 10000)
     hist.Draw()
 
     # Set axis titles
-    hist.GetXaxis().SetTitle('x axis')
-    hist.GetYaxis().SetTitle('y axis')
+    hist.GetXaxis().SetTitle("x axis")
+    hist.GetYaxis().SetTitle("y axis")
 
     # Add the ATLAS Label
     astyle.ATLASLabel(0.2, 0.87, "Internal")
